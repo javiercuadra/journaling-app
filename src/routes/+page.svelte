7815916248b34f80a24ede55662b636617aa5ed4
Javier Cuadra <32556567+javiercuadra@users.noreply.github.com>
2025-11-19 17:22:22 -0500

    create file structure for drafting pipeline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2 class="text-4xl text-blue-600 font-bold">Tailwind works!</h2>
